28b5f72a0bc6945fa0cca15050f08d4283b0e329
fenglin <huaiyinnianshao28@163.com>
2023-02-20 20:53:50 +0800

    Guava Partition

--- src/main/java/com/guava/PartitionDemo.java
+++ src/main/java/com/guava/PartitionDemo.java
@@ -7,7 +7,7 @@ import org.apache.commons.collections.CollectionUtils;
 import java.util.ArrayList;
 import java.util.List;
 
-public class NewArrayListDemo {
+public class PartitionDemo {
 
     public static void main(String[] args) {
         testPartition();
@@ -26,13 +26,14 @@ public class NewArrayListDemo {
         List<String> list = Lists.newArrayList("lin","chen","wu","zhang","qiu");
         //分页，每页2个元素
         List<List<String>> partitionList = Lists.partition(list, 2);
-        if (CollectionUtils.size(partitionList) > 1) {
-            //下标是从0开始的，获取第2页的元素，先判断集合的size，避免数组越界。
-            List<String> partList = partitionList.get(1);
-            //结果为 [wu, qiu]
+        if (CollectionUtils.size(partitionList) > 0) {
+            //下标是从0开始的,先判断集合的size，避免数组越界。
+            List<String> partList = partitionList.get(0);
+            //结果为 [lin, chen]
             System.out.println(partList);
         }
     }
+
 
 
 }
